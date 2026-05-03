40a40a76a8af570d6289257227331ca773ea3316
Ivy Leano <ivyleano@IMM.local>
2026-05-03 15:34:51 -0400

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,44 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+window.APP_CONSTANTS = {
+  tabTitle: "",
+  navigation: [ "Introduction", "Impacts", "Notre Opinion", "Preuve", "Conclusion", "Chanson", "Biblio" ],
+  pageHeading: "Les impacts de notre consommation des ressources non renouvelables",
+  introTitle: "Consommer ou préserver ? Les ressources non renouvelables",
+  introParagraph: "Les ressources non renouvelables, comme le pétrole, le gaz naturel et le charbon, sont très importantes au Canada. Elles sont utilisées pour produire de l’énergie, faire fonctionner les voitures et soutenir les industries. Cependant, ces ressources ne peuvent pas être remplacées rapidement, car elles prennent des millions d’années à se former. Aujourd’hui, leur utilisation soulève plusieurs questions importantes.",
+  impactsTitle: "Impacts",
+  positiveImpactsTitle: "Impacts positifs",
+  positiveImpactsParagraph: "Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.",
+  negativeImpactsTitle: "Impacts négatifs",
+  negativeImpactsParagraph: " Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.",
+  conclusionsTitle: "Conclusion de la page",
+  conclusionsParagraph: "Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.",
+  opinionsTitle: "Notre Opinion",
+  argumentsTitle: "Les Arguments",
+  argumentsParagraph: "Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.",
+  explicationTitle: "Explication",
+  explicationParagraph: "Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.",
+  conclusionsSectionTitle: "Conclusion",
+  conclusionsSectionParagraph: "Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.Les ressources non renouvelables ont des avantages, mais leurs impacts négatifs sont très importants, surtout pour l’environnement.",  
+  biblioTitle: "Bibliographic",
+  biblioParagraph: [
+    "https://www.projetecolo.com/pollution-petroliere-causes-et-consequences-388.html",
+    "https://www.alterna-energie.fr/blog-article/epuisement-des-ressources-non-renouvelables",
+    "https://www.alloprof.qc.ca/fr/eleves/bv/sciences/les-ressources-energetiques-renouvelables-et-non-s1383",
+    "https://www.projetecolo.com/epuisement-des-ressources-naturelles-causes-et-consequences-163.html",
+    "https://fr.wikipedia.org/wiki/Ressource_non_renouvelable",
+    "https://fr.scienceaq.com/Nature/100109934.html",
+    "https://frontieredevie.fr/source-denergie-non-renouvelable"
+  ],
+  preuvesTitle: "Preuves",
+  preuvesOne: "Elles produisent du CO₂, ce qui réchauffe la planète",
+  preuvesTwo: "Elles prennent des millions d’années à se former",
+  preuvesThree: "Les énergies renouvelables sont de meilleures solutions",
+  chansonTitle: "Chanson"
+};
+
+
+                        
+                        
+                       
